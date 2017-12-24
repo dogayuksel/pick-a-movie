f05750b968137c3618cba81dec7d0c6d7ccdd619
Doga Yuksel <dogayuksel@gmail.com>
2017-12-24 22:36:37 +0100

    Remove este message and firebase logo from the login page

--- src/browser/auth/LoginPage.react.js
+++ src/browser/auth/LoginPage.react.js
@@ -20,19 +20,6 @@ export default class LoginPage extends Component {
         <FormattedMessage {...linksMessages.login}>
           {message => <Helmet title={message} />}
         </FormattedMessage>
-        <h2>
-          <a href="https://www.firebase.com/" target="_blank">
-            <img
-              role="presentation"
-              src={require('../firebase/logo.png')}
-              style={{ height: 27, width: 140 }}
-            />
-          </a>
-        </h2>
-        <p>
-          Este uses <a href="https://firebase.google.com/docs/auth/">
-          Firebase Authentication</a> because it saves a lot of time.
-        </p>
         <Login location={location} />
       </div>
     );
